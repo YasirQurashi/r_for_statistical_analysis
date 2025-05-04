c6e5bc7c1c5afb098ec2b82c7c0e8670308bc9cd
Muhammad Yasir Qurashi <qureshiyasir335@gmail.com>
2025-05-04 10:09:26 +0500

    Add files via upload

---
+++ day_01.R
@@ -0,0 +1,15 @@
+# How to install R and Rstudio
+# Difference between R and Rstudio
+# Basic Syntax of R
+# 1. R is case sensitive
+# 2. R is a vectorized language
+# 3. R is a functional programming language
+
+# naming rules for file and project
+
+# 1. No capital letters
+# 2. No spacing
+# 3. Only dash and underscore
+# i.e day_01.R  ,  day-01.R , day_01.
+
+ 
